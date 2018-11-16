0171400cba2030266034fcb605b10c854abcd8a3
tweety1212 <tweety880402@gmail.com>
2018-11-16 11:51:24 +0800

    第10項 1.switch觸發人物走動

---
+++ Assets/cara.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cara : MonoBehaviour {
+    Animator ani;
+    float speed = 0;
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
