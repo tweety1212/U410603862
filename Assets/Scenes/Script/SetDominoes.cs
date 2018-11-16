2745bfba67a72d46ac433e59be768b6391045d3c
tweety1212 <tweety880402@gmail.com>
2018-11-16 10:37:02 +0800

    第4項 1.生成10個骨牌

--- Assets/Scenes/Script/SetDominoes.cs
+++ Assets/Scenes/Script/SetDominoes.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class SetDominoes : MonoBehaviour {
 	public GameObject dominoePrefab;
 	public GameObject dominoeEnd;
-	public int NumCenterDom = 25;
+    public GameObject dominoestart;
+    public int NumCenterDom = 25;
 	void Start() {
-		Vector3 dir = dominoeEnd.transform.position - transform.position;
+		Vector3 dir = dominoeEnd.transform.position - dominoestart.transform.position;
 		float interval = dir.magnitude / (NumCenterDom+1);
 		dir.Normalize();
 		for (int i=1; i<=NumCenterDom; i++){
 			GameObject newDom = (GameObject)Instantiate(dominoePrefab);
-			newDom.transform.position = transform.position + dir*interval*i;
-			newDom.transform.rotation = transform.rotation;
+			newDom.transform.position = dominoestart. transform.position + dir*interval*i;
+			newDom.transform.rotation = dominoestart.transform.rotation;
 		}
 	}
 }
